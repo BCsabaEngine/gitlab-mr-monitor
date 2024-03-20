b4ecdcb9adc68ac4b9cf1734ed810eae3a6b0079
Csaba Balázs <csababalazs@ip-192-168-10-143.eu-central-1.compute.internal>
2024-03-20 11:25:51 +0100

    v1.3.0

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,7 +2,7 @@ const GHPATH = 'https://bcsabaengine.github.io/gitlab-mr-monitor/';
 
 const APP_PREFIX = 'gitlab-mr-monitor_';
 
-const VERSION = 'v1.2.12';
+const VERSION = 'v1.3.0';
 
 // The files to make available for offline use. make sure to add others to this list
 const URLS = [
